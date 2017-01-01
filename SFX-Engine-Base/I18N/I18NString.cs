f305b279ac4fe355ac8fad41904aa7806be9f3bd
Jeff Sweeney <jeffpublic@kintoshmalae.com>
2017-01-02 02:28:06 +1000

    Basic Translation Structure
    Completed migration to SecondLanguage for i18n services.

--- SFX-Engine-Base/I18N/I18NString.cs
+++ SFX-Engine-Base/I18N/I18NString.cs
@@ -1,16 +1,51 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Resources;
 using System.Text;
+using SecondLanguage;
 
 namespace com.kintoshmalae.SFXEngine.I18N {
     public sealed class I18NString {
-        private I18NString() {}
+        private static readonly I18NString Instance = new I18NString();
+
+        private readonly Translator T = Translator.Default;
+        private ResourceManager rm;
+
+        private I18NString() {
+            T.RegisterTranslationsByCulture(@"I18N\{0}\*.po");
+            T.RegisterTranslation(@"I18N\en-au\sfx-showplayer.po");
+        }
+
+        string LookupRM(string id) {
+            // Lazy load the ResourceManager, but only load it once.
+            if (rm == null) rm = new ResourceManager("Messages", typeof(I18NString).Assembly);
+            return rm.GetString(id);
+        }
+
+        string LookupRM(string id, params string[] p) {
+            return String.Format(CultureInfo.CurrentCulture, LookupRM(id), p);
+        }
+
+        string LookupPO(string id) {
+            return T[id];
+        }
+
+        string LookupPO(string id, params string[] p) {
+            return T[id, p];
+        }
 
         public static string Lookup(string id) {
-            ResourceManager rm = new ResourceManager("Messages", typeof(I18NString).Assembly);
-            return rm.GetString(id);
+            string _result = Instance.LookupPO(id);
+            if (_result == null) _result = Instance.LookupRM(id);
+            return _result;
+        }
+
+        public static string LookupWithParameters(string id, params string[] p) {
+            string _result = Instance.LookupPO(id, p);
+            if (_result == null) _result = Instance.LookupRM(id, p);
+            return _result;
         }
     }
 }
